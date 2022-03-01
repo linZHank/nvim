039ee7b8c9ff7eace85369b97850dbfc978f0f63
linzhank <linzhank@gmail.com>
2022-02-28 23:58:58 -0600

    add lsp

--- init.lua
+++ init.lua
@@ -3,4 +3,5 @@ require "pbd0.keymaps"
 require "pbd0.plugins"
 require "pbd0.colorscheme"
 require "pbd0.completion"
+require "pbd0.lsp"
 
---
+++ lua/pbd0/lsp/init.lua
@@ -0,0 +1,7 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+    return
+end
+
+require("pbd0.lsp.installer")
+require("pbd0.lsp.handlers").setup()
